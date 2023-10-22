a8b6ea6a82158302e0f199b4a7e6f053326c05cf
shiyenhqy <146970477+shiyenhqy@users.noreply.github.com>
2023-10-22 14:46:09 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
+//Rock-Paper-Scissors
+//An Implementation of the Game
+//Shiyen Yang shhyang@ucsc.edi
+// October 2023
 
 // Functions
 
@@ -19,3 +18,19 @@ function main() {
 
 // let's get this party started
 main();
+
+// find a friend (over the internet)
+	// make buttons for “rock”, “paper”, and “scissors”
+// have countdown 
+	// (start loop) start the game :D
+// begin countdown for choosing one button
+// each player picks a button but their choice is hidden from the other player
+// button selection is locked once countdown ends
+// print “rock”, then “paper”, then “scissors” on the screen
+	// print “shoot” on the screen
+	// print both player’s pick onto the screen
+	// check for winner and loser
+		// winner = ROCK when rock and scissor, SCISSOR when scissor and paper, PAPER when paper and rock
+		// loser = ROCK when rock and paper, SCISSOR when scissor and rock, PAPER when paper and scissors
+		// if both players pick the same thing, loop! 
+// congratulate the winner
