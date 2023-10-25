3b16e09e9e95390b968efa7c3f7b993a1ed9c81d
shiyenhqy <146970477+shiyenhqy@users.noreply.github.com>
2023-10-25 08:41:37 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,26 @@
+/**
+ * lab 5
+ * @author Shiyen Yang <shhyang@ucsc.edu>
+ * @date October 2023
+ */
+
+
+// Define Variables
+var make="Toyota";
+var model="Matrix";
+var color="Silver";
+var year=2006;
+
+// Own
+var ownIt=false;
+
+// Calculate
+var age = 2023 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Do I own it: " + ownIt + "<br>");
+document.writeln("Age: " + age + " years<br>");
