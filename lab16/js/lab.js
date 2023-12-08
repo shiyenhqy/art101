450bf6ca4fb5efa110fd535c18bdb7f6be9a6d9a
shiyenhqy <146970477+shiyenhqy@users.noreply.github.com>
2023-12-07 22:29:24 -0800

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,40 +3,37 @@
 // Lab 16
 
 // Using the core $.ajax() method
-$.ajax({
-    // The URL for the request (replace with your API endpoint)
-    url: "https://xkcd.com/info.0.json",
-    // The data to send (adjust based on your API requirements)
-    data: { 
-        id: 123,
-        api_key: "blahblahblah",
-    },
-    // Whether this is a POST or GET request
-    type: "GET",
-    // The type of data we expect back
-    dataType: "json",
-    // What do we do when the API call is successful
-    success: function(comicObj) {
-        // Process the parts and add them to your webpage
-        // Example: Assuming you have a div with an id of "comic-container" in your HTML
-        $("#comic-container").html(`
-            <h2>${comicObj.title}</h2>
-            <img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">
-        `);
-    },
-    // What we do if the API call fails
-    error: function(jqXHR, textStatus, errorThrown) { 
-        // Handle errors
-        console.log("Error:", textStatus, errorThrown);
-    }
-});
+
+//help of chatgpt to randomize comic number
+function getRandomComicNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function fetchAndDisplayComic() {
+    // Generate a random comic number
+    const randomComicNumber = getRandomComicNumber(1, 2500); // Adjust the range based on the XKCD comics available
+// help form chatgpt to write how to do the random comic number
+    $.ajax({
+        url: `https://xkcd.com/${randomComicNumber}/info.0.json`,
+        dataType: "json",
+        success: function(comicObj) {
+            // Display the random comic on the webpage
+            $("#output").html(`
+                <h2>${comicObj.title}</h2>
+                <img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">
+            `);
+        },
+        error: function(jqXHR, textStatus, errorThrown) { 
+            console.log("Error:", textStatus, errorThrown);
+        }
+    });
+}
 
 // Add event listener to the button
 $(document).ready(function() {
+    fetchAndDisplayComic();
+
     $("#showComicButton").on("click", function() {
-        // Trigger the AJAX call again when the button is clicked
-        $.ajax({
-            // Your AJAX settings here...
-        });
+        fetchAndDisplayComic();
     });
 });
